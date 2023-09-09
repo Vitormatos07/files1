33bf17cd39d77647ee011c1f5b073caf40376707
Vitormatos07 <vitormatos9870@gmail.com>
2023-09-09 09:13:49 -0300

    Initial implementation

---
+++ Files/Program.cs
@@ -0,0 +1,16 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
